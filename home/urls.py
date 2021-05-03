9829fd8e5c4c87fc619ff01a042406d87c17be57
ninchy0 <minmar666@gmail.com>
2021-05-03 18:21:06 +0545

    .

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('portfolio', portfolio, name='portfolio'),
     path('price', price, name='price'),
     path('services', services, name='services'),
+    path('blog-home', blog_home, name='blog-home'),
+    path('blog-single', blog_single, name='blog-single'),
+    path('elements', elements, name='elements')
 ]
